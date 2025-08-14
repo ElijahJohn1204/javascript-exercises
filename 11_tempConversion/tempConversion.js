e0dc66f0c14fda63d1fa3ffc1d029ac3e52f9e06
ElijahJohn1204 <elijahtjohn@gmail.com>
2025-08-14 00:28:27 -0500

    Completed Exercise 11
    Wrote a couple functions to convert temperatures between Celsius and Fahrenheit.
    Wrote helper function to round to nearest tenth using Math library.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return roundToNearestTenth((temp - 32) / 1.8)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return roundToNearestTenth((temp * 1.8) + 32)
 };
 
+const roundToNearestTenth = function(number) {
+  return Math.round(number * 10) / 10
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
