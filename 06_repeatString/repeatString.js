a3e0f6667c46db2691d9b32c1b59bd107b05556d
ElijahJohn1204 <elijahtjohn@gmail.com>
2025-08-14 00:02:22 -0500

    Completed Exercise 6
    wrote function that takes string and number and returns a string with the input string number amount of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num == 0) {
+        return "";
+    } else if (num < 0) {
+        return 'ERROR';
+    }
 
+    let i = 1;
+    let output = str;
+    while (i < num) {
+        output += str;
+        i++;
+    }
+    return output;
 };
 
 // Do not edit below this line
