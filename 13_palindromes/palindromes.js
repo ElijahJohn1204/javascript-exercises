44ba6a6bf665836df02557166169d22453a60ec8
ElijahJohn1204 <elijahtjohn@gmail.com>
2025-08-21 15:58:08 -0500

    A bit trickier, checked strings for palindromes
    Used alphanumeric characters only and involved converting string into array and back to use certain built-in functions

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz1234567890'
 
+    const clean = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const cleanReverse = clean.split('').reverse().join('');
+
+    return clean == cleanReverse
 };
 
 // Do not edit below this line
