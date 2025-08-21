88acfd73bf054e6732288449dd7b9bc9d60de7f2
ElijahJohn1204 <elijahtjohn@gmail.com>
2025-08-21 16:07:56 -0500

    Completed Exercise 14
    Calculated fibonacci numbers given index. Accepts string and int values. Calculates using three variables and loop.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    const index = parseInt(fib);
+    if (index < 0) return 'OOPS';
 
+    let prev = 0;
+    let current = 1;
+    if (index == 0) return prev;
+    if (index == 1) return current;
+
+    let sum = prev + current;
+    for (let i = 2; i < index; i++) {
+        prev = current;
+        current = sum;
+        sum = prev + current;
+    }
+    return sum;
 };
 
 // Do not edit below this line
